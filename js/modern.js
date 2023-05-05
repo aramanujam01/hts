b5d58f8ac56074d93c1a559c3dc46a16ed95a46b
aramanujam01 <98789212+aramanujam01@users.noreply.github.com>
2023-05-05 05:11:19 -0400

    v 1.0 done

--- js/modern.js
+++ js/modern.js
@@ -1,100 +1,52 @@
-const data = {
-  "Disease": {
-    "Major Threat": 79,
-    "Minor Threat": 19,
-    "Not a Threat": 2
-  },
-  "Terrorism": {
-    "Major Threat": 73,
-    "Minor Threat": 25,
-    "Not a Threat": 2
-  },
-  "Nuclear Weapons": {
-    "Major Threat": 73,
-    "Minor Threat": 23,
-    "Not a Threat": 3
-  },
-  "Cyberattacks": {
-    "Major Threat": 72,
-    "Minor Threat": 25,
-    "Not a Threat": 3
-  },
-  "China": {
-    "Major Threat": 62,
-    "Minor Threat": 29,
-    "Not a Threat": 7
-  }
-};
+// set the dimensions and marginLines of the graph
+var marginLine = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 1000 - marginLine.left - marginLine.right,
+    height = 500 - marginLine.top - marginLine.bottom;
 
-// Step 1: Define dimensions
-const width = 1000;
-const height = 600;
-const margin = { top: 0, right: 100, bottom: 30, left: 40 };
-
-// Step 2: Create SVG element
-var svg = d3.select("#us-threat")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+// append the svg object to the body of the page
+var svgLine = d3.select("#my_dataviz2")
+  .append("svg")
+    .attr("width", width + marginLine.left + marginLine.right)
+    .attr("height", height + marginLine.top + marginLine.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform",
+          "translate(" + marginLine.left + "," + marginLine.top + ")");
 
-// Step 3: Define scales
-var xScale = d3.scaleBand()
-  .domain(Object.keys(data))
-  .range([0, width])
-  .padding(0.2);
+//Read the data
+d3.csv("data/deaths.csv",
 
-var yScale = d3.scaleLinear()
-  .domain([0, 100])
-  .range([height, 0]);
+  // When reading the csv, I must format variables:
+  function(d){
+    return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+  },
 
-// Step 4: Create axes
-var xAxis = d3.axisBottom(xScale)
-  .tickSize(0)
-  .tickPadding(10);
+  // Now I can use this dataset:
+  function(data) {
 
-// Step 5: Create color scale
-var colorScale = {
-  'Major Threat': "red",
-  "Minor Threat": "blue",
-  "Not a Threat": "green"
-}
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svgLine.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
 
-// Add category labels
-svg.selectAll(".category-label")
-  .data(Object.keys(data))
-  .enter().append("text")
-  .attr("class", "category-label")
-  .attr("x", function(d) { return xScale(d) + xScale.bandwidth() / 2; })
-  .attr("y", height + margin.bottom / 2)
-  .style("text-anchor", "middle")
-  .text(function(d) { return d; });
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(data, function(d) { return +d.value; })])
+      .range([ height, 0 ]);
+    svgLine.append("g")
+      .call(d3.axisLeft(y));
 
-// Step 6: Add bars to chart
-svg.selectAll("rect")
-.data(Object.keys(data))
-.enter().append("rect")
-.attr("x", function(d) { return xScale(d); })
-.attr("y", function(d) { return yScale(data[d]["Major Threat"]); })
-.attr("width", xScale.bandwidth())
-.attr("height", function(d) { return height - yScale(data[d]["Major Threat"]); })
-.attr("fill", colorScale["Major Threat"]);
+    // Add the line
+    svgLine.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "#6C2D2B")
+      .attr("stroke-width", 3)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
 
-svg.selectAll("rect")
-.data(Object.keys(data))
-.enter().append("rect")
-.attr("x", function(d) { return xScale(d); })
-.attr("y", function(d) { return yScale(data[d]["Minor Threat"]); })
-.attr("width", xScale.bandwidth())
-.attr("height", function(d) { return height - yScale(data[d]["Minor Threat"]); })
-.attr("fill", colorScale["Minor Threat"]);
-
-svg.selectAll("rect")
-.data(Object.keys(data))
-.enter().append("rect")
-.attr("x", function(d) { return xScale(d); })
-.attr("y", function(d) { return yScale(data[d]["Not a Threat"]); })
-.attr("width", xScale.bandwidth())
-.attr("height", function(d) { return height - yScale(data[d]["Not a Threat"]); })
-.attr("fill", colorScale["Not a Threat"]);
-
+})
